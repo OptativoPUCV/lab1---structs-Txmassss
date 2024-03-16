cb65852ab3da317332ba71b78561dc09f9aa3307
Your Name <you@example.com>
2024-03-16 15:48:58 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -110,7 +110,7 @@ int checkSorted(int arr[], int size)
   int contAs = 0;
   int contDes = 0;
   
-  for(int i = 0; i <= size; i++)
+  for(int i = 0; i < size; i++)
     {
       if(arr[i]<=arr[i+1])
         contAs++;
